769a5323cb35ce0e5c5ea7f5e600afe1f0eb1fd5
Becky828 <38361070+Becky828@users.noreply.github.com>
2024-03-13 22:33:31 -0700

    Restored comments.

--- CMakeS24CS696_PS1_p5/pagerank_template-1.cpp
+++ CMakeS24CS696_PS1_p5/pagerank_template-1.cpp
@@ -1,5 +1,3 @@
-//#include "matplotlibcpp.h"
-#include <matplot/matplot.h>
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -9,9 +7,13 @@
 #include <set>
 #include <cmath>
 
-// Custom comparison function to compare pairs based on the second value
-//bool compareSecond(std::pair<int, double>& a, std::pair<int, double>& b) {
+//Under consideration for 10 EC pts
+// 
+//Please note, our solution fixes sink nodes by adding edges from the sink nodes to all other nodes in the graph. Additionally, the outdegree of the sink nodes is 
+// increased by the number of nodes in the graph, excepting itself. This is done to ensure that the graph is strongly connected 
+// and that the pagerank algorithm converges. Each sink node is also added to the in degree map of all other nodes.
 
+// Custom comparison function to compare pairs based on the second value
 bool compareSecond(const std::pair<int, double>& a, const std::pair<int, double>& b) {
 	return a.second > b.second;
 }
@@ -21,11 +23,13 @@ bool compareSecond(const std::pair<int, double>& a, const std::pair<int, double>
 std::vector<int> sinkFinder(std::set<int> nodes, std::map<int, int> o, std::map<int, int> i) {
 
 	std::vector<int> sinks;
-
 	for (int node : nodes) {
 
+		//A sink is a node with no out degree
+		// So, if a node is in the in degree map but not in the out degree map, it is a sink.
 		if (i.contains(node) && !o.contains(node)) {
 
+			//Ensure that the sink is not already in the vector
 			int cnt = std::count(sinks.begin(), sinks.end(), node);
 
 			// Check if the target value was found 
@@ -40,8 +44,7 @@ std::vector<int> sinkFinder(std::set<int> nodes, std::map<int, int> o, std::map<
 
 }
 
-// Function to sort the map according 
-// to value in a (key-value) pairs 
+// Function to sort the map according to value in a (key-value) pairs 
 std::vector<std::pair<int, double> > sort(std::map<int, double> M)
 {
 	// Declare vector of pairs 
@@ -60,7 +63,7 @@ std::vector<std::pair<int, double> > sort(std::map<int, double> M)
 	return A;
 }
 
-// Print the first thousand entries of the multimap
+//Ensure that the sink is not already in the vector
 void topThousandPrinter(std::vector<std::pair<int, double> >  V) {
 
 	//Initialize the counter
@@ -74,7 +77,7 @@ void topThousandPrinter(std::vector<std::pair<int, double> >  V) {
 	}
 }
 
-//Gets the ids of the top 5 pagerank nodes
+//A function that gets the IDs of the top 5 pagerank nodes
 std::vector<int> topFiveIDGetter(std::vector<std::pair<int, double> >  V) {
 
 	// Initialize the counter
@@ -95,6 +98,7 @@ std::vector<int> topFiveIDGetter(std::vector<std::pair<int, double> >  V) {
 	}
 	return B;
 }
+
 // Function to load file
 std::fstream fileLoader(char fn[]) {
 
@@ -124,9 +128,7 @@ void fileCloser(std::fstream& f) {
 
 
 
-//Writes each node and its pagerank to a text file line line by line in decreasing order of pagerank
-//void pageRankWriter(std::map<int, double>   V) {
-
+//A function that writes each node and its pagerank to a text file line line by line in decreasing order of pagerank
 void pageRankWriter(std::vector<std::pair<int, double> > V) {
 
 	char filename[] = "pagerank_out.txt";
@@ -141,47 +143,54 @@ void pageRankWriter(std::vector<std::pair<int, double> > V) {
 	fileCloser(file);
 }
 
+//A function that writes output for the top 5 nodes in their respective text output files
 void topFivePageRankWriter(std::vector<double> V1, std::vector<double> V2, std::vector<double> V3,
 	std::vector<double> V4, std::vector<double> V5) {
 
+	//Initializes top five file names
 	char filename_top_first[] = "top_first_out.txt";
 	char filename_top_second[] = "top_second_out.txt";
 	char filename_top_third[] = "top_third_out.txt";
 	char filename_top_fourth[] = "top_fourth_out.txt";
 	char filename_top_fifth[] = "top_fifth_out.txt";
 
+	//Loads the top five files
 	std::fstream file1 = fileLoader(filename_top_first);
 	std::fstream file2 = fileLoader(filename_top_second);
 	std::fstream file3 = fileLoader(filename_top_third);
 	std::fstream file4 = fileLoader(filename_top_fourth);
 	std::fstream file5 = fileLoader(filename_top_fifth);
 
+	//Writes the text output for the top first node
 	for (double v1 : V1) {
 
 		file1 << v1 << std::endl;
 	}
 
+	//Writes the text output for the top second node
 	for (double v2 : V2) {
 
 		file2 << v2 << std::endl;
 	}
 
+	//Writes the text output for the top third node
 	for (double v3 : V3) {
 
 		file3 << v3 << std::endl;
 	}
 
+	//Writes the text output for the top fourth node
 	for (double v4 : V4) {
 
 		file4 << v4 << std::endl;
 	}
 
+	//Writes the text output for the top fifth node
 	for (double v5 : V5) {
 
 		file5 << v5 << std::endl;
 	}
 
-	//file.close();
 	fileCloser(file1);
 	fileCloser(file2);
 	fileCloser(file3);
@@ -191,15 +200,11 @@ void topFivePageRankWriter(std::vector<double> V1, std::vector<double> V2, std::
 
 
 int main() {
-	using namespace matplot;
-
-	// ifstream file;
-		// file.open("web-Stanford.txt");
 
 	std::ifstream file("web-Stanford.txt"); // Change "edges.txt" to your file name
 
+	//Test file
 	//	std::ifstream file("web-Stanford_very_abridged.txt"); // Change "edges.txt" to your file name
-
 
 	if (!file.is_open()) {
 		std::cerr << "Unable to open file" << std::endl;
@@ -217,8 +222,13 @@ int main() {
 		}
 	}
 
+	//Initializes data structures required for page rank algorithms
 	std::map<int, std::vector<int>> edges;
+
+	//For regular page rank
 	std::map<int, double> pagerank, previous_pagerank;
+
+	//For top five page rank
 	std::map<int, double> top_five_pagerank, top_five_previous_pagerank;
 	std::set<int> nodes;
 	std::map<int, int> out_degree, in_degree;
@@ -236,37 +246,44 @@ int main() {
 
 	file.close();
 
+	//Gets a vector containing the sinks
 	std::vector<int> sink_nodes = sinkFinder(nodes, out_degree, in_degree);
 
-
-	//std::map<int, int> ::iterator it4;
+	//Required for fixing sink nodes
 	int all_other_nodes = nodes.size() - 1;
 
 	for (int s : sink_nodes) {
+
+		//Clarifies then duty of s
 		from_id = s;
 
+		//Updates each sink nodes outdegree by adding all other nodes to the sink node's out degree
 		int current_out_degree = out_degree[from_id];
 		current_out_degree += (all_other_nodes);
 		out_degree[from_id] = current_out_degree;
 
-		//for (auto i = in_degree.begin(); i != in_degree.end(); i++){
 
-
+		//Fixes the sink node issue by adding edges from the sink nodes to all other nodes in the graph
 		for (int node : nodes) {
 			if (node != s) {
 
+				//Clarifies the duty of node
 				to_id = node;
 
+				//Adds all other nodes as a sink node's edges
 				edges[to_id].push_back(from_id);
 
 				int current_in_degree = in_degree[to_id];
 
+				//Updates the in degree of all other nodes by adding the sink node to their in degree
 				current_in_degree++;
 				in_degree[to_id] = current_in_degree;
 			}
 		}
 
 	}
+
+	//Initiales some variables required for page rank algorithm
 	double epsilon = 0.15, one_minus_epsilon = 1 - epsilon, avg_error = 0.00001;
 	int n = nodes.size();
 
@@ -282,21 +299,32 @@ int main() {
 
 	//A
 	// Implement the pagerank algorithm here
+
+		//Incrementally performs 1 to T iterations
 	for (int t = 1; t < num_iterations + 1; t++)
 	{
+		//Copies the most recent page rank map
 		previous_pagerank = pagerank;
+
+		//Incrementally iterates through the nodes at locations indexed 1 to n
 		for (int node : nodes)
 		{
 			double sum = 0;
+
+			//Incrementally iterates through the edges of a given node
 			for (int from_node : edges[node])
 			{
+				//Performs a summation of the quotient resulting from the previous page rank divided by the outdegree of the from node
 				sum += previous_pagerank[from_node] / out_degree[from_node];
 			}
+
+			//Updates the page rank by evaluating the sum with the damping factor
 			pagerank[node] = one_minus_epsilon * sum + avg_epsilon;
 		}
 
 	}
 
+	//Gets a vector of the page ranks sorted in descending ordr
 	std::vector<std::pair<int, double> >  sorted = sort(pagerank);
 
 	// Print the pagerank of the top 1000 nodes
@@ -327,24 +355,26 @@ int main() {
 
 	std::map<int, double> ::iterator it3;
 
-	// Plot the latest pageranks of the top 5 nodes
-
+	// Store the latest pageranks of the top 5 nodes in the respective vector
 	int top_first = topFiveIds[0];
 	int top_second = topFiveIds[1];
 	int top_third = topFiveIds[2];
 	int top_fourth = topFiveIds[3];
 	int top_fifth = topFiveIds[4];
-
 	std::vector<int> t_index;
 
+	//Incrementally performs 1 to T iterations
 	for (int t = 1; t < num_iterations + 1; t++)
 	{
 		t_index.push_back(t);
 
+		//Copies the most recent page rank map
 		top_five_previous_pagerank = top_five_pagerank;
 
+		//Iterate through the most recent page rank map
 		for (it3 = top_five_previous_pagerank.begin(); it3 != top_five_previous_pagerank.end(); it3++) {
 
+			//Collect the relevant information
 			int current_node = it3->first;
 			double current_pagerank = it3->second;
 
@@ -355,18 +385,27 @@ int main() {
 			// Check if the target value was found 
 			if (cnt > 0) {
 
+				//Adds the most recent top first page rank to the first vector
 				if (current_node == top_first) {
 					y_1.push_back(current_pagerank);
 				}
+
+				//Adds the most recent top second page rank to the second vector
 				else if (current_node == top_second) {
 					y_2.push_back(current_pagerank);
 				}
+
+				//Adds the most recent top third page rank to the third vector
 				else if (current_node == top_third) {
 					y_3.push_back(current_pagerank);
 				}
+
+				//Adds the most recent top fourth page rank to the fourth vector
 				else if (current_node == top_fourth) {
 					y_4.push_back(current_pagerank);
 				}
+
+				//Adds the most recent top fifth page rank to the fifth vector
 				else if (current_node == top_fifth) {
 					y_5.push_back(current_pagerank);
 				}
@@ -374,27 +413,28 @@ int main() {
 			}
 		}
 
+		//Incrementally iterates through the nodes at locations indexed 1 to n
 		for (int node : nodes)
 		{
 			double sum = 0;
+
+			//Incrementally iterates through the edges of a given node
+
 			for (int from_node : edges[node])
 			{
+				//Performs a summation of the quotient resulting from the previous page rank divided by the outdegree of the from node
 				sum += top_five_previous_pagerank[from_node] / out_degree[from_node];
 			}
+
+			//Updates the page rank by evaluating the sum with the damping factor
 			top_five_pagerank[node] = one_minus_epsilon * sum + avg_epsilon;
 		}
 
 	}
 
-	//plot(t_index, y_1)->line_width(2).color("red");
-	//plot(t_index, y_1)->color({ 0.f, 0.7f, 0.9f });
-	/*plot(x_2, y_2)->line_width(2).color("blue");
-	plot(x_3, y_3)->line_width(2).color("green");
-	plot(x_4, y_4)->line_width(2).color("yellow");
-	plot(x_5, y_5)->line_width(2).color("black");*/
-
-	//show();
+	//Submits the vectors for the top five page rank nodes to the function designated to write them to respective text output files
 	topFivePageRankWriter(y_1, y_2, y_3, y_4, y_5);
 
+	//Returns code 0 when the program runs and exits properly with no errors
 	return 0;
 }
